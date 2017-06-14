c7d6f3b808b234838ad53f627686b423940ef36f
Joseph Hennessey <4jp.hennessey@gmail.com>
2017-06-14 14:01:43 -0400

    minor formatting changes

--- src/test/java/pageobjects/BaseClass.java
+++ src/test/java/pageobjects/BaseClass.java
@@ -7,80 +7,81 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BaseClass {
-	public static WebDriver driver;
+    public static WebDriver driver;
 
-	public BaseClass(WebDriver driver){
-		this.driver = driver;
-		initPage(driver);
-	}
-	public void initPage(WebDriver driver) {
+    public BaseClass(WebDriver driver) {
+        this.driver = driver;
+        initPage(driver);
+    }
+
+    public void initPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
-	//----------- H E A D E R   L I N K S ----------------------------------------|
-	private static final String XPATH_headerPrefix = "//div[@id='nav-wrapper']";
+    //----------- H E A D E R   L I N K S ----------------------------------------|
+    private static final String XPATH_headerPrefix = "//div[@id='nav-wrapper']";
 
-	@FindBy(xpath = "//div[@id='logo-wrapper']/a")
+    @FindBy(xpath = "//div[@id='logo-wrapper']/a")
     @CacheLookup
     public static WebElement LINK_TOP_returnHome;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='ABOUT US']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='ABOUT US']")
     @CacheLookup
     public static WebElement LINK_TOP_aboutUs;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='DEVELOPERS']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='DEVELOPERS']")
     @CacheLookup
     public static WebElement LINK_TOP_developers;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='BLOG']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='BLOG']")
     @CacheLookup
-	public static WebElement LINK_TOP_blog;
+    public static WebElement LINK_TOP_blog;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='WIZARDS WANTED']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='WIZARDS WANTED']")
     @CacheLookup
-	public static WebElement LINK_TOP_careers;
+    public static WebElement LINK_TOP_careers;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='PRESS']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='PRESS']")
     @CacheLookup
-	public static WebElement LINK_TOP_press;
+    public static WebElement LINK_TOP_press;
 
-	@FindBy(xpath = XPATH_headerPrefix + "//a[text()='CONTACT']")
+    @FindBy(xpath = XPATH_headerPrefix + "//a[text()='CONTACT']")
     @CacheLookup
-	public static WebElement LINK_TOP_contact;
+    public static WebElement LINK_TOP_contact;
 
-	//----------- F O O T E R   L I N K S ---------------------------------------------->
-	private static final String XPATH_footerPrefix = "//div[@id='footer-nav-wrapper']";
+    //----------- F O O T E R   L I N K S ---------------------------------------------->
+    private static final String XPATH_footerPrefix = "//div[@id='footer-nav-wrapper']";
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='ABOUT US']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='ABOUT US']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_aboutUs;
+    public static WebElement LINK_BOTTOM_aboutUs;
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='DEVELOPERS']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='DEVELOPERS']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_developers;
+    public static WebElement LINK_BOTTOM_developers;
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='BLOG']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='BLOG']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_blog;
+    public static WebElement LINK_BOTTOM_blog;
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='WIZARDS WANTED']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='WIZARDS WANTED']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_careers;
+    public static WebElement LINK_BOTTOM_careers;
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='PRESS']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='PRESS']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_press;
+    public static WebElement LINK_BOTTOM_press;
 
-	@FindBy(xpath = XPATH_footerPrefix + "//a[text()='CONTACT']")
+    @FindBy(xpath = XPATH_footerPrefix + "//a[text()='CONTACT']")
     @CacheLookup
-	public static WebElement LINK_BOTTOM_contact;
+    public static WebElement LINK_BOTTOM_contact;
 
-	//----------- l e g a l    l i n k s  ----------------------------------------|
-	@FindBy(linkText = "privacy policy")
+    //----------- l e g a l    l i n k s  ----------------------------------------|
+    @FindBy(linkText = "privacy policy")
     @CacheLookup
-	public static WebElement LINK_privacyPolicy;
+    public static WebElement LINK_privacyPolicy;
 
-	@FindBy(linkText = "terms of use")
+    @FindBy(linkText = "terms of use")
     @CacheLookup
-	public static WebElement LINK_termsOfUse;
+    public static WebElement LINK_termsOfUse;
 }
--- src/test/java/pageobjects/PageMessages.java
+++ src/test/java/pageobjects/PageMessages.java
@@ -10,42 +10,45 @@ import java.util.List;
 
 public class PageMessages extends BaseClass {
 
-	public PageMessages(WebDriver driver) {
-		super(driver);
-	}
+    public PageMessages(WebDriver driver) {
+        super(driver);
+    }
 
-	private static final String expectedThankYouMessage = "thanks for reaching out...";
-	private List getWordLengths(String message) {
-		List<Integer> result = new ArrayList<Integer>();
-		int index = 0;
-		for (String word: message.split(" ")) {
-			result.add(word.length());
-		}
-		return result;
-	}
-	private String CSS_thankYouCharPattern = "#thankyou h1 .word%d span.char%d";
-	private void waitForCompleteMessage(WebDriver driver, List<Integer> wordLengths) {
-		int totalWords = wordLengths.size();
-		WebDriverWait wait = new WebDriverWait(driver, 1, 50);
-		for (int wordIndex=1; wordIndex<=totalWords; wordIndex++) {
-			Integer length = wordLengths.get(wordIndex-1);
-			for (int charIndex=1; charIndex<=length; charIndex++) {
-				String css = String.format(CSS_thankYouCharPattern, wordIndex, charIndex); // length);
-				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(css)));
-			}
-		}
-	}
+    private List getWordLengths(String message) {
+        List<Integer> result = new ArrayList<Integer>();
+        int index = 0;
+        for (String word : message.split(" ")) {
+            result.add(word.length());
+        }
+        return result;
+    }
 
-	private By BY_thankYouMessage = By.cssSelector("#thankyou h1");
-	public String getThankYouMessage() {
-		WebDriverWait wait = new WebDriverWait(driver, 5);
-		List<Integer> expectedWordLengths = getWordLengths(expectedThankYouMessage);
-		waitForCompleteMessage(driver, expectedWordLengths);
-		String result = driver.findElement(BY_thankYouMessage).getText();
-		return result;
-	}
+    private String CSS_thankYouCharPattern = "#thankyou h1 .word%d span.char%d";
+
+    private void waitForCompleteMessage(WebDriver driver, List<Integer> wordLengths) {
+        int totalWords = wordLengths.size();
+        WebDriverWait wait = new WebDriverWait(driver, 1, 50);
+        for (int wordIndex = 1; wordIndex <= totalWords; wordIndex++) {
+            Integer length = wordLengths.get(wordIndex - 1);
+            for (int charIndex = 1; charIndex <= length; charIndex++) {
+                String css = String.format(CSS_thankYouCharPattern, wordIndex, charIndex); // length);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(css)));
+            }
+        }
+    }
+
+    private static final String expectedThankYouMessage = "thanks for reaching out...";
+    private By BY_thankYouMessage = By.cssSelector("#thankyou h1");
+
+    public String getThankYouMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        List<Integer> expectedWordLengths = getWordLengths(expectedThankYouMessage);
+        waitForCompleteMessage(driver, expectedWordLengths);
+        String result = driver.findElement(BY_thankYouMessage).getText();
+        return result;
+    }
 }
-		
+
 
 	
 	
